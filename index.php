1cb5780b96b85ca7369b1834f0968d421bb43be3
Wladimir Delenclos <wdelenclos@gmail.com>
2020-06-18 17:02:32 +0200

    Add chatbot

--- index.php
+++ index.php
@@ -37,6 +37,7 @@
 
 	<!-- Load Bludit Plugins: Site head -->
 	<?php Theme::plugins('siteHead'); ?>
+	<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="61ea5bfb-3dac-4a0b-b5e7-afc574f3d1e5";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
 </head>
 <body>
 
